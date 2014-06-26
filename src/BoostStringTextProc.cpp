3c0486638b002e9b87deea1b49038964dd33a01a
fsziegler <fsziegler@gmail.com>
2014-06-26 14:52:13 -0700

    Added timers to Array test that time iterating through the arrays. Added boost_system and boost_chrono library linking Added the following to C/C++ General:Preprocessor Include Paths, Macros etc.:[tab]Providers: CDT GCC Build Output Parser, CDT GCC Managed Build Setting Entries, and CDT User Setting Entries. This fixes an Indexer problem.

--- src/BoostStringTextProc.cpp
+++ src/BoostStringTextProc.cpp
@@ -1,16 +1,86 @@
 #include "BoostStringTextProc.h"
-#include <iostream>
-#include <memory>
-#include <assert.h>
+
+// <Array>
+#include <boost/array.hpp>
+#include <array>
+// </Array>
+
+// <Chrono>
+#include <boost/chrono.hpp>
+#include <boost/chrono/duration.hpp>
+#include <boost/chrono/system_clocks.hpp>
+//#include <boost/type_traits.hpp>
+// </Chrono>
+
+// <Random>
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/generator_iterator.hpp>
+
+typedef boost::minstd_rand base_generator_type;
+// </Random>
 
 using namespace std;
-//cout << "Perform Array tests" << endl;
+using namespace boost;
 
 void Array() {
-   assert(__cplusplus >= 201103L);
-   int len = 7;
-   auto text = std::unique_ptr<char[]>(new char[len]);
+   const size_t len = 1E6;
+   typedef boost::array<float, (const size_t) len>::iterator BAryItr;
+   typedef boost::array<float, (const size_t) len>::reverse_iterator BAryRItr;
+   typedef std::array<float, (const size_t) len>::iterator AryItr;
+   typedef std::array<float, (const size_t) len>::reverse_iterator AryRItr;
+
    cout << "Perform Array tests" << endl;
+   cout << "Testing Boost & C++11 Array" << endl;
+   boost::array<float, (const size_t) len> boost_tst_array;
+   std::array<float, (const size_t) len> tst_array;
+   cout << "Boost Array size = " << boost_tst_array.size() << endl;
+   cout << "C++11 Array size = " << tst_array.size() << endl;
+
+   base_generator_type generator(42);
+   boost::uniform_real<> uni_dist(0, 1);
+   boost::variate_generator<base_generator_type&, boost::uniform_real<> > uni(
+         generator, uni_dist);
+
+   boost::chrono::duration<double> sec;
+   chrono::steady_clock::time_point start = chrono::steady_clock::now();
+   // Forward Iteration Test
+   std::cout.setf(std::ios::fixed);
+   for (size_t i = 0; i < len; ++i) {
+      boost_tst_array[i] = uni();
+      tst_array[i] = uni();
+   }
+
+   size_t i(0);
+   for (BAryItr itr = boost_tst_array.begin(); itr < boost_tst_array.end();
+         ++itr) {
+      ++i;
+   }
+   assert(len == i);
+   size_t j(0);
+   for (AryItr itr = tst_array.begin(); itr < tst_array.end(); ++itr) {
+      ++j;
+   }
+   assert(len == j);
+   sec = boost::chrono::steady_clock::now() - start;
+   cout << "Forward Array iterations succeeded [took " << sec.count() << " seconds]" << endl;
+
+   // Reverse Iteration Test
+   for (BAryRItr itr = boost_tst_array.rbegin(); itr < boost_tst_array.rend();
+         ++itr) {
+      --i;
+   }
+   assert(0 == i);
+   for (AryRItr itr = tst_array.rbegin(); itr < tst_array.rend();
+         ++itr) {
+      --j;
+   }
+   assert(0 == j);
+   sec = boost::chrono::steady_clock::now() - start;
+   cout << "Reverse Array iterations succeeded [took " << sec.count() << " seconds]" << endl;
+
 }
 
 void Bind() {
