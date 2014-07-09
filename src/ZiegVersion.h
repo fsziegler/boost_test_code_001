7a89ae52cdfcd8f5bf1bdb9e39c8c22f0949a6ef
fsziegler <fsziegler@gmail.com>
2014-07-09 06:53:18 -0700

    Renamed PNG file Edit

--- src/ZiegVersion.h
+++ src/ZiegVersion.h
@@ -39,11 +39,11 @@ namespace ZiegVersion
 
 // These are updated automatically with each build
 
-   const long int BuildNumber = 1625;
-   const long int UTCSeconds = 1404693186;   // Updated with date +%s
+   const long int BuildNumber = 1628;
+   const long int UTCSeconds = 1404694558;   // Updated with date +%s
    const string BuildDate = "2014-07-06";    // Updated with date +%F
-   const string BuildTime = "17:33:06";      // Updated with date +%T
-   const string UUID = "436bdb76-056e-11e4-9d53-e7147a8f13ec"; // Updated with uuid
+   const string BuildTime = "17:55:58";      // Updated with date +%T
+   const string UUID = "7559a14c-0571-11e4-aea5-4b17d9790df4"; // Updated with uuid
    
    string GetFullVersionString() {
       stringstream fullVersion;
